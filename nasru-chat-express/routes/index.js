d925157e6465b33e35118c8ae08eec23769d65f0
nashahgit <nashah25@gmail.com>
2016-03-22 16:49:10 +0530

    Sec

--- nasru-chat-express/routes/index.js
+++ nasru-chat-express/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Nasruddin' });
+    res.render('index', { title: 'Nasruddin Shah' });
 });
 
 module.exports = router;
