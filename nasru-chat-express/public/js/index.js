fb48b24a6f1eaf6312c6d25ca2a95c2481c298ef
nashahgit <nashah25@gmail.com>
2016-03-24 21:01:31 +0530

    fghfg

--- nasru-chat-express/public/js/index.js
+++ nasru-chat-express/public/js/index.js
@@ -1,12 +1,14 @@
 ﻿
 var socket = io();
+var user = 'Nasru: ';
 $('#send-message-btn').click(function () {
     var msg = $('#message-box').val();
     socket.emit('chat', msg);
-    $('#messages').append($('<p>').text(msg));
+    $('#messages').append($('<p>').text(user + msg));
     $('#message-box').val('');
     return false;
 });
+
 socket.on('chat', function (msg) {
     $('#messages').append($('<p>').text(msg));
 });
